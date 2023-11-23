8c46ff4d157a693abc9ad2b79b15021ace9f1cbf
Enriquenf07 <enriqueferreira956@gmail.com>
2023-11-23 20:50:28 -0300

    dev-01

--- src/Produto.java
+++ src/Produto.java
@@ -1,4 +1,41 @@
 
 public class Produto {
+    private String id;
+    private String nome;
+    private double preco;
+    private int qntd;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQntd() {
+        return qntd;
+    }
+
+    public void setQntd(int qntd) {
+        this.qntd = qntd;
+    }
+
     
 }
