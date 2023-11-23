58705230f8c682b664dfc563467bcf2adf8ca3f2
thiagobrnrd <thiagobgomes20@gmail.com>
2023-11-23 20:47:24 -0300

    Comanda

--- src/Comanda.java
+++ src/Comanda.java
@@ -8,5 +8,30 @@
  * @author enrique
  */
 public class Comanda {
+    private String id;
+    private Arraylist<Produto> produtos = new Arraylist<>();
+    private double valorF;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @return the produtos
+     */
+    public Arraylist<Produto> getProdutos() {
+        return produtos;
+    }
+
+    /**
+     * @return the valorF
+     */
+    public double getValorF() {
+        return valorF;
+    }
+    
     
 }
